afef538ddb12d1bfc2ed01db9b510dadbd1a798f
Txeim <tarunsharma2376@gmail.com>
2026-07-05 23:34:39 +0530

    second try

--- Array/Two pointer/Leetcode125.java
+++ Array/Two pointer/Leetcode125.java
@@ -1,17 +1,16 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        String lower = s.toLowerCase();
-        String S="";
-        for(int i=0;i<lower.length();i++){
-            if(Character.isLetterOrDigit(lower.charAt(i))){
-                S=S+lower.charAt(i);
+        StringBuilder sb = new StringBuilder();
+        for(char c:s.toCharArray()){
+            if(Character.isLetterOrDigit(c)){
+                sb.append(Character.toLowerCase(c));
             }
         }
         int left=0;
-        int right=S.length()-1;
+        int right=sb.length()-1;
         boolean ans=true;
         while(right>left){
-            if(S.charAt(left)!=S.charAt(right) ){
+            if(sb.charAt(left)!=sb.charAt(right) ){
                 ans=false;
             }
             right--;
